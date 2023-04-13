f00ce63371e518af3c638acef71fce91ba049f30
Nicholas Scott <nscott6@huskers.unl.edu>
2023-04-12 23:36:21 -0500

    Added more unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,105 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
-    }
+		// Subtraction test
+		[Test]
+		public void TestSubtraction_Scott()
+		{
+			Assert.AreEqual(1, Program.Subtraction("3", "2"));
+		}
+
+		// Multiplication test
+		[Test]
+		public void TestMultiplication_Scott()
+		{
+			Assert.AreEqual(6, Program.Multiplication("2", "3"));
+		}
+
+		// Division test
+		[Test]
+		public void TestDivision_Scott()
+		{
+			Assert.AreEqual(2, Program.Division("6", "3"));
+		}
+
+		// Modulo test
+		[Test]
+		public void TestModulo_Scott()
+		{
+			Assert.AreEqual(1, Program.Modulo("5", "2"));
+		}
+
+		// Subtraction invalid
+		[Test]
+		public void TestSubtraction_Invalid_Scott()
+		{
+			Assert.Throws<FormatException>(() => Program.Subtraction("3", "a"));
+			Assert.Throws<FormatException>(() => Program.Subtraction("a", "3"));
+			Assert.Throws<FormatException>(() => Program.Subtraction("a", "a"));
+		}
+
+		// Subtraction null
+		[Test]
+		public void TestSubtraction_Null_Scott()
+		{
+			Assert.Throws<ArgumentNullException>(() => Program.Subtraction("3", null));
+			Assert.Throws<ArgumentNullException>(() => Program.Subtraction(null, "3"));
+			Assert.Throws<ArgumentNullException>(() => Program.Subtraction(null, null));
+		}
+
+		// Multiplication invalid
+		[Test]
+		public void TestMultiplication_Invalid_Scott()
+		{
+			Assert.Throws<FormatException>(() => Program.Multiplication("2", "a"));
+			Assert.Throws<FormatException>(() => Program.Multiplication("a", "2"));
+			Assert.Throws<FormatException>(() => Program.Multiplication("a", "a"));
+		}
+
+		// Multiplication null
+		[Test]
+		public void TestMultiplication_Null_Scott()
+		{
+			Assert.Throws<ArgumentNullException>(() => Program.Multiplication("2", null));
+			Assert.Throws<ArgumentNullException>(() => Program.Multiplication(null, "2"));
+			Assert.Throws<ArgumentNullException>(() => Program.Multiplication(null, null));
+		}
+
+		// Division invalid
+		[Test]
+		public void TestDivision_Invalid_Scott()
+		{
+			Assert.Throws<FormatException>(() => Program.Division("6", "a"));
+			Assert.Throws<FormatException>(() => Program.Division("a", "6"));
+			Assert.Throws<FormatException>(() => Program.Division("a", "a"));
+		}
+
+		// Division null
+		[Test]
+		public void TestDivision_Null_Scott()
+		{
+			Assert.Throws<ArgumentNullException>(() => Program.Division("6", null));
+			Assert.Throws<ArgumentNullException>(() => Program.Division(null, "6"));
+			Assert.Throws<ArgumentNullException>(() => Program.Division(null, null));
+		}
+
+		// Modulo invalid
+		[Test]
+		public void TestModulo_Invalid_Scott()
+		{
+			Assert.Throws<FormatException>(() => Program.Modulo("5", "a"));
+			Assert.Throws<FormatException>(() => Program.Modulo("a", "5"));
+			Assert.Throws<FormatException>(() => Program.Modulo("a", "a"));
+		}
+
+		// Modulo null
+		[Test]
+		public void TestModulo_Null_Scott()
+		{
+			Assert.Throws<ArgumentNullException>(() => Program.Modulo("5", null));
+			Assert.Throws<ArgumentNullException>(() => Program.Modulo(null, "5"));
+			Assert.Throws<ArgumentNullException>(() => Program.Modulo(null, null));
+		}
+	}
 }
+
